d9c9421b0ea5040b8be73d021c7dea01da9612f6
Youssef hamad <youssefhamad007@gmail.com>
2025-09-11 22:01:02 +0300

    first

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,6 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    host: "::",
-    port: 8080,
-  },
   plugins: [react()],
   base:"/CoffeeNess",   // 👈 no lovable-tagger
   resolve: {
